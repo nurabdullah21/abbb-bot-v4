0bec365c9e6690df9f1e13a3a42742ca8087ad2e
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-24 04:23:21 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -12,13 +12,13 @@ let tag = `https://wa.me/qr/XHSNF7KVOZCRI1`
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let name = `@${m.sender.split`@`[0]}`
 
-let str = `Hai kak ${name}
+let str = `Hai Kak ${name}
 
 Saya YUDHA BOT, Bot Whatsapp Yang Menyusahkan Kamu Untuk Mempersulit Sesuatu Seperti Membuat Sticker Dan Lainnya, Ada Butuh Info Dariku?`
 let isi =`Note : Perlakukan Bot Secara Baik dan Bijak. Bot ini terdapat Anti Spam yang berupa cooldown command selama *5 Detik* Setiap kali pemakaian.`
 let wibu = `https://api-reysekha.herokuapp.com/api/wallpaper/islami?apikey=APIKEY` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.send2ButtonLoc(m.chat, await conn.resize('https://api-reysekha.herokuapp.com/api/wallpaper/islami?apikey=APIKEY', 300, 300), str, isi, 'MENU BOT', '.listmenu', 'ATURAN BOT', '.aturan', m)
+conn.send2ButtonLoc(m.chat, await conn.resize('https://api-reysekha.herokuapp.com/api/wallpaper/islami?apikey=APIKEY', 300, 300), str, isi, 'COMMAND', '.listmenu', 'RULES', '.aturan', m)
 
 }
 handler.help = ['menu', 'help', '?']
