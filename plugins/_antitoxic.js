eec933cf4f5fef1af110edb3b553dde8cb4aafcb
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-24 10:46:13 +0700

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -13,7 +13,6 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
     if (chat.antiToxic && isAntiToxic) {
         await conn.sendButton(m.chat, `*Kata Tak Pantas Terdeteksi!*
         
-        Warning: ${user.warning} / 10
 Jika warning mencapai 10 kamu akan *dibanned+kick*
 
 “Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).${isBotAdmin ? '' : '\n\n_Bot bukan Admin_'}`, author, ['Matikan Fitur', '/disable antitoxic'], m)
