f1bd57a2520184be1ffa281693ab8f8937fefd19
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-25 04:43:42 +0700

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -3,20 +3,27 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //let handler = async(m, { conn, text, usedPrefix, command }) => {
 //let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
 
-let str = `*────── 「 DONATE 」 ──────*
-Hai 👋
-Kalian bisa mendukung saya agar bot ini tetap up to date dengan SCAN BARCODE diatas menggunakan Aplikasi DANA !
-Berapapun donasi kalian akan sangat berarti 👍
+let str = `*── 「 RULES 」 ──*
 
-DANA [${global.pdana}]
+1. Jangan Spam Bot. 
+Sanksi: *WARN/SOFT BLOCK*
 
-𝐀𝐫𝐢𝐠𝐚𝐭𝐨𝐮!!!
-Contact person Owner:
-wa.me/6283155997772 (Owner)`
+2. Jangan Vc Bot. 
+Sanksi: *SOFT BLOCK*
+ 
+3. Jangan Mengeksploitasi Bot.
+Sanksi: *PERMANENT BLOCK*
+
+4. Jangan Telepon Bot.
+Sanksi: *SOFT BLOCK*
+
+Jika Sudah Dipahami Rulesnya, Silakan Ketik *.menu* Untuk Memulai!
+
+*── 「 YUDHA BOT 」 ──*`
 let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 let imgnya = 'https://telegra.ph/file/73dc6b92751ec9608c10b.jpg'
-conn.sendFile(m.chat, imgnya, str, str, 0)
+conn.sendMessage(m.chat, imgnya, str, str, 0)
 }
 handler.help = ['donasi']
 handler.tags = ['info']
