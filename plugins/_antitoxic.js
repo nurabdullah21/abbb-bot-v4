d938abbeb0c35bb8631df3786eab174f17118b05
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-24 10:20:26 +0700

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,35 +1,7 @@
 const isToxic = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|a(su|sw|syu)/i // tambahin sendiri
 
 export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe && isOwner) return !0
-    let chat = db.data.chats[m.chat]
-    let user = db.data.users[m.sender]
-    let isBadword = badwordRegex.exec(m.text)
-
-    if (!chat.antiBadword && !chat.isBanned && !user.banned && isBadword && !isOwner && !isAdmin) {
-        user.warning += 1
-        this.send2Button(m.chat, `*Badword terdeteksi!*
-Warning: ${user.warning} / 10
-Jika warning mencapai 10 kamu akan *dibanned+kick*
-
-ketik *#on antibadword* untuk menyalakan antibadword
-ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
-
-“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, wm, 'MATIKAN ANTIBADWORD', '.off antibadword', 'ASTAGHFIRULLAH', '.maaf', m)
-        if (user.warning >= 10) {
-            user.banned = true
-            if (m.isGroup) {
-                if (isBotAdmin) {
-                	this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-                   //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-                }
-            }
-        }
-    }
-    return !0
-  
-  
-    /*if (m.isBaileys && m.fromMe)
+  if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
@@ -39,15 +11,15 @@ ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
     let bang = m.key.id
     
     if (chat.antiToxic && isAntiToxic) {
-        await conn.sendButton(m.chat, `*Kata Aneh Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antitoxic', '/disable antitoxic'], m)
+        await conn.sendMessage(m.chat, `*Kata Kasar Terdeteksi!*\n\nWarning: ${user.warning} / 10\nJika warning mencapai 10 kamu akan *dibanned+kick*\n\n“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim). ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, m)
         if (isBotAdmin && bot.restrict) {
             // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
     global.db.data.users[m.sender].warn += 1
     global.db.data.users[m.sender].banned = true
     return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-        } else if (!bot.restrict) return m.reply('Semoga harimu suram!')
+        } else if (!bot.restrict) return m.reply('Semoga harimu suram & cepat sadar!')
     }
-    return !0*/
+    return !0
 }
 
 export const disable = true
