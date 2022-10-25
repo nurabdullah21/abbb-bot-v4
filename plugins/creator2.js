986d1b8aa41af20770fb4eb9e5fe42a324aeb7b4
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-25 07:28:20 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -7,8 +7,8 @@ let name = await conn.getName(who)
 
 if (command == 'whmods1') {
  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner YUDHA;BOT;;Md\nFN:Owner YUDHA BOT\nNICKNAME:👑 Owner YUDHA BOT\nORG:Owner\nTITLE:MODS\nitem1.TEL;waid=6283155997772:+62 831-5599-7772\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.youtube.com\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:flasherdeveloper@gmail.com\nitem3.X-ABLabel:💌 Mail Owner YUDHA BOT\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 28 Juli\nEND:VCARD`
-const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
-let caption = `👋 Hai Kak @${who.split("@")[0]}\n\nItu Nomer Ownerkun Mau Tau Soal Apa Ya?`
+const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: 0 })
+let caption = `👋 Hai Kak @${who.split("@")[0]}\n\nItu Nomer Ownerku, Mau Tau Soal Apa Ya?`
 let itu = `Jangan Sungkan Chat Ya Kak`
     await conn.sendButton(m.chat, caption, itu, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
 }
