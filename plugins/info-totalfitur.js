d3d4bed8d33a60180107a58e8bb89134443bdb41
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-25 13:42:50 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -3,17 +3,7 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], m, {
-contextInfo: { externalAdReply :{
-                        mediaUrl: '',
-                        mediaType: 2,
-                        description: 'anu',
-                        title: bottime,
-                        body: 'Total Cintaku Padamu',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: sig
-                      }}
-})
+ await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm, giflogo, [['MENU','.menu']], m )
 }
 
 
