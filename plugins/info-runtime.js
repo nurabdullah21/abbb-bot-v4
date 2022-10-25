3f7ddaa4cc5d96f4a0b1d8cfd14da53137cb8406
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-25 08:17:42 +0700

    Update info-runtime.js

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,4 +1,29 @@
-let { MessageType } = (await import('@adiwajshing/baileys')).default
+import fs from 'fs'
+let handler = async (m, { conn, args, command }) => {
+	let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+ conn.reply(m.chat, `${htki} *R U N T I M E* ${htka}\n${muptime}\n`, m, {
+contextInfo: { externalAdReply :{
+                        mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: bottime,
+                        body: wm2,          previewType: 0,
+                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        sourceUrl: snh
+                      }}
+})
+}
+
+
+/*let { MessageType } = (await import('@adiwajshing/baileys')).default
 
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
   let type = (args[0] || '').toLowerCase()
@@ -68,7 +93,7 @@ const listMessage = {
   } catch (err) {
     m.reply("Error\n\n\n" + err.stack)
   }
-}
+}*/
 
 handler.help = ['runtime']
 handler.tags = ['info']
@@ -76,3 +101,12 @@ handler.command = ['runtime', 'rt']
 
 
 export default handler
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+}
+
