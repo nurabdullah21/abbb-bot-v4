b70518fbf81bd5771f04d053b5729c777637e09f
nurabdullah21 <116748343+nurabdullah21@users.noreply.github.com>
2022-10-27 04:31:46 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -28,4 +28,4 @@ export default handler
 
 
 
-//BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner ABBB;BOT;;Md\nFN:Owner ABBB BOT\nNICKNAME:👑 Owner YUDHA BOT\nORG:Owner\nTITLE:MODS\nitem1.TEL;waid=6285156858655:+62 851-5685-8655\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.youtube.com\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:flasherdeveloper@gmail.com\nitem3.X-ABLabel:💌 Mail Owner ABBB BOT\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 28 Juli\nEND:VCARD
+//BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner ABBB;BOT;;Md\nFN:Owner ABBB BOT\nNICKNAME:👑 Owner ABBB BOT\nORG:Owner\nTITLE:MODS\nitem1.TEL;waid=6285156858655:+62 851-5685-8655\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.youtube.com\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:flasherdeveloper@gmail.com\nitem3.X-ABLabel:💌 Mail Owner ABBB BOT\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 28 Juli\nEND:VCARD
