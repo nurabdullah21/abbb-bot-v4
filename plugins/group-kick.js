820aba1465477b0c66b81f4f0cc1a0ba1a88a3d7
flasherdeveloper <71258374+flasherdeveloper@users.noreply.github.com>
2022-10-26 13:33:46 +0700

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -8,13 +8,12 @@ let handler = async (m, { conn, participants }) => {
             kickedUser.concat(res)
             await delay(1 * 1000)
         }
-    m.reply(`Succes kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
+    m.reply(`OTW kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
 
 }
 handler.help = ['kick'].map(v => v + ' @user')
 handler.tags = ['group']
 handler.command = /^(kick)$/i
-handler.owner = true
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
